721f6d1866498148db33716107e4918a95901290
Artem <samtsevichartem@gmail.com>
2022-12-22 12:05:24 +0100

    Removed everything related to the powerlevel

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,7 +1,3 @@
-    # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-    # Initialization code that may require console input (password prompts, [y/n]
-    # confirmations, etc.) must go above this block; everything else may go below.
-
     # If you come from bash you might have to change your $PATH.
     # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
@@ -134,4 +130,5 @@
     ZSH_CUSTOM_AUTOUPDATE_QUIET=true
     # TheFuck plugin
     eval $(thefuck --alias)
+
 

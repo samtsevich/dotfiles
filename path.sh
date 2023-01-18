7fc7f88a2ed1f1376b5495a158c61ca731a3b6a2
Artem <samtsevichartem@gmail.com>
2023-01-18 12:55:45 +0100

    Pyenv path fix

--- path.sh
+++ path.sh
@@ -3,12 +3,16 @@
 ##################
 
 # Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+PYENV_ROOT="$HOME/.pyenv"
 
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+if [[ -d $PYENV_ROOT ]]; then
+  export PYENV_ROOT=$PYENV_ROOT
+  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+
+  export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+fi
 
 ##################
 ###### LIBS ######

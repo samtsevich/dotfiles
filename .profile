f0941e4efbb7b81ea1cdffb2a0e84ec779c393f9
Artem <samtsevichartem@gmail.com>
2023-05-15 20:56:30 +0200

    Simplification of sourcing files

--- .profile
+++ .profile
@@ -4,6 +4,5 @@ LC_ALL=en_US.utf-8
 
 [[ -f ~/.dotfiles/common/aliases.sh ]]  && source ~/.dotfiles/common/aliases.sh
 [[ -f ~/.dotfiles/common/apps.sh ]]     && source ~/.dotfiles/common/apps.sh
-[[ -f ~/.dotfiles/common/path.sh ]]     && source ~/.dotfiles/common/path.sh
 
 [[ -f ~/.dotfiles/local.sh ]] && source ~/.dotfiles/local.sh
--- common/apps.sh
+++ common/apps.sh
@@ -1,6 +1,9 @@
 # [[ $MYSHELL == *"zsh"* ]] && echo "Equal" || echo "Not equal"
 MYSHELL=`echo $SHELL`
 
+export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
+
+# Zoxide exclude dirs
 export _ZO_EXCLUDE_DIRS="/opt/intel/*:/work/opt/intel/*"
 
 # Zoxide and FZF
@@ -28,3 +31,15 @@ fi
 
 # Starship
 [[ -f ~/.dotfiles/common/starship.sh ]] && source ~/.dotfiles/common/starship.sh
+
+# Pyenv
+PYENV_ROOT="$HOME/.pyenv"
+
+if [[ -d $PYENV_ROOT ]]; then
+  export PYENV_ROOT=$PYENV_ROOT
+  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+
+  export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+fi

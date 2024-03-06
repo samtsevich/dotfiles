281e951929fe86b86644e7aa41b35564d641c470
Artem <samtsevichartem@gmail.com>
2024-03-06 10:30:05 +0100

    Added one more key for openSUSE logo

--- common/starship.sh
+++ common/starship.sh
@@ -24,29 +24,29 @@ fi
 # set an icon based on the distro
 # make sure your font is compatible with https://github.com/lukas-w/font-logos
 case $_distro in
-    *kali*)                  ICON="ﴣ";;
-    *arch*)                  ICON="";;
-    *debian*)                ICON="";;
-    *raspbian*)              ICON="";;
-    *ubuntu*)                ICON="";;
-    *elementary*)            ICON="";;
-    *fedora*)                ICON="";;
-    *coreos*)                ICON="";;
-    *gentoo*)                ICON="";;
-    *mageia*)                ICON="";;
-    *centos*)                ICON="";;
-    *opensuse*|*tumbleweed*) ICON="";;
-    *sabayon*)               ICON="";;
-    *slackware*)             ICON="";;
-    *linuxmint*)             ICON="";;
-    *alpine*)                ICON="";;
-    *aosc*)                  ICON="";;
-    *nixos*)                 ICON="";;
-    *devuan*)                ICON="";;
-    *manjaro*)               ICON="";;
-    *rhel*)                  ICON="";;
-    *macos*)                 ICON="";;
-    *)                       ICON="";;
+    *kali*)                          ICON="ﴣ";;
+    *arch*)                          ICON="";;
+    *debian*)                        ICON="";;
+    *raspbian*)                      ICON="";;
+    *ubuntu*)                        ICON="";;
+    *elementary*)                    ICON="";;
+    *fedora*)                        ICON="";;
+    *coreos*)                        ICON="";;
+    *gentoo*)                        ICON="";;
+    *mageia*)                        ICON="";;
+    *centos*)                        ICON="";;
+    *opensuse*|*sles*|*tumbleweed*)  ICON="";;
+    *sabayon*)                       ICON="";;
+    *slackware*)                     ICON="";;
+    *linuxmint*)                     ICON="";;
+    *alpine*)                        ICON="";;
+    *aosc*)                          ICON="";;
+    *nixos*)                         ICON="";;
+    *devuan*)                        ICON="";;
+    *manjaro*)                       ICON="";;
+    *rhel*)                          ICON="";;
+    *macos*)                         ICON="";;
+    *)                               ICON="";;
 esac
 
 export STARSHIP_DISTRO="$ICON"
